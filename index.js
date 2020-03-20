5bd4ce8e6a56b76576fa332c455a99e16d42daa0
Jonny Burger <jonathanburger11@gmail.com>
2020-03-20 09:52:49 +0100

    !

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2020,
     sourceType: "module",
     ecmaFeatures: {
       jsx: true
